977ea35be78874590be73bfdde9c3c4aa46d2c23
pxgamer <owzie123@gmail.com>
2018-10-16 21:38:09 +0100

    Add initial class for CoinAPI constants

---
+++ src/CoinAPI.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace pxgamer\CoinAPI;
+
+/**
+ * Class CoinAPI
+ */
+class CoinAPI
+{
+    /**
+     * The base URI for the CoinAPI main site.
+     */
+    public const BASE_URI = 'https://coinapi.io';
+    /**
+     * The base URI for the CoinAPI API.
+     */
+    public const API_BASE_URI = 'https://rest.coinapi.io/v1/';
+}
