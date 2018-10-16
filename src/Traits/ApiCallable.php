f1d292afd5e10754a16c70ff14a871634c6edbe4
pxgamer <owzie123@gmail.com>
2018-10-16 21:44:14 +0100

    Add an ApiCallable trait for API calls

---
+++ src/Traits/ApiCallable.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace pxgamer\CoinAPI\Traits;
+
+use GuzzleHttp\Client;
+use GuzzleHttp\ClientInterface;
+use pxgamer\CoinAPI\CoinAPI;
+
+/**
+ * Trait ApiCallable
+ */
+trait ApiCallable
+{
+    /**
+     * @var ClientInterface
+     */
+    protected $client;
+    /**
+     * @var string
+     */
+    protected $apiKey;
+
+    /**
+     * ApiCallable constructor.
+     * @param string $apiKey
+     */
+    public function __construct(string $apiKey)
+    {
+        $this->apiKey = $apiKey;
+
+        $this->client = new Client([
+            'base_uri' => CoinAPI::API_BASE_URI,
+            'headers'  => [
+                'X-CoinAPI-Key' => $this->apiKey,
+                'Content-Type'  => 'application/json',
+            ],
+        ]);
+    }
+
+    /**
+     * @param string $endpoint
+     *
+     * @return mixed
+     */
+    public function call(string $endpoint)
+    {
+        return \GuzzleHttp\json_decode(
+            $this->client
+                ->get($endpoint)
+                ->getBody()
+                ->getContents()
+        );
+    }
+}
