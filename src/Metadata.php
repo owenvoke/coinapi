2c993b676d002c7999b08438f425ce7d9198fb8f
pxgamer <owzie123@gmail.com>
2018-10-16 23:19:48 +0100

    Add Metadata class for metadata endpoints

---
+++ src/Metadata.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace pxgamer\CoinAPI;
+
+/**
+ * Class Metadata
+ */
+class Metadata
+{
+    use Traits\ApiCallable;
+
+    /**
+     * Retrieve a detailed list of all exchanges.
+     *
+     * @return array
+     */
+    public function getExchanges(): array
+    {
+        return $this->call('exchanges');
+    }
+
+    /**
+     * Retrieve a detailed list of all assets.
+     *
+     * @return array
+     */
+    public function getAssets(): array
+    {
+        return $this->call('assets');
+    }
+
+    /**
+     * Retrieve a detailed list of all symbols.
+     *
+     * @param array $filterSymbolIds
+     * @return array
+     */
+    public function getSymbols(array $filterSymbolIds = []): array
+    {
+        $data = [
+            'filter_symbol_id' => implode(';', $filterSymbolIds),
+        ];
+
+        return $this->call('symbols'.http_build_query($data));
+    }
+}
