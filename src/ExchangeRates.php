6a205794c59a90a176150718d1c2bfb1365f92f0
pxgamer <owzie123@gmail.com>
2018-10-16 23:31:45 +0100

    Add ExchangeRates class for rates endpoints

---
+++ src/ExchangeRates.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace pxgamer\CoinAPI;
+
+/**
+ * Class ExchangeRates
+ */
+class ExchangeRates
+{
+    use Traits\ApiCallable;
+
+    /**
+     * Retrieve the current exchange rate between requested asset and all other assets.
+     *
+     * @param string   $assetIdBase
+     * @param string[] $filterAssetIds
+     * @return \stdClass
+     */
+    public function getExchangeRates(string $assetIdBase, array $filterAssetIds = []): \stdClass
+    {
+        $data = [
+            'filter_symbol_id' => implode(';', $filterAssetIds),
+        ];
+
+        return $this->call('exchangerate/'.strtoupper($assetIdBase).'?'.http_build_query($data));
+    }
+
+    /**
+     * Retrieve exchange rate between pair of requested assets at specific or current time.
+     *
+     * @param string      $assetIdBase
+     * @param string      $assetIdQuote
+     * @param string|null $time
+     * @return \stdClass
+     */
+    public function getExchangeRate(string $assetIdBase, string $assetIdQuote, string $time = null): \stdClass
+    {
+        $data = [
+            'time' => $time,
+        ];
+
+        return $this->call(
+            'exchangerate/'.strtoupper($assetIdBase).'/'.strtoupper($assetIdQuote).
+            '?'.http_build_query($data)
+        );
+    }
+}
